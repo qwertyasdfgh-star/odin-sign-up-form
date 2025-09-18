e41057cd55778abdb19c96b8a57f60de149b808e
qwertyasdfgh-star <idgaf9899@gmail.com>
2025-09-18 11:23:39 +0800

    .

--- script.js
+++ script.js
@@ -1,41 +1,38 @@
 AOS.init({ duration: 800, once: true });
-        feather.replace();
-        function togglePwd() {
-            const pwd = document.getElementById('pwd');
-            const eyeOpen = document.getElementById('eyeOpen');
-            const eyeClosed = document.getElementById('eyeClosed');
-            if (pwd.type === 'password') {
-                pwd.type = 'text';
-                eyeOpen.classList.add('hidden');
-                eyeClosed.classList.remove('hidden');
-            } else {
-                pwd.type = 'password';
-                eyeOpen.classList.remove('hidden');
-                eyeClosed.classList.add('hidden');
-            }
-        }
-        document.getElementById('signupForm').addEventListener('submit', e => {
-            e.preventDefault();
-            alert('Account created successfully! (This is just a demo.)');
-        });
 
-AOS.init({ duration: 800, once: true });
-        feather.replace();
-        function toggleCnfrmPwd() {
-            const pwd = document.getElementById('cnfrmPwd');
-            const eyeOpen = document.getElementById('eyeOpen1');
-            const eyeClosed = document.getElementById('eyeClosed1');
-            if (pwd.type === 'password') {
-                pwd.type = 'text';
-                eyeOpen.classList.add('hidden');
-                eyeClosed.classList.remove('hidden');
-            } else {
-                pwd.type = 'password';
-                eyeOpen.classList.remove('hidden');
-                eyeClosed.classList.add('hidden');
-            }
-        }
-        document.getElementById('signupForm').addEventListener('submit', e => {
-            e.preventDefault();
-            alert('Account created successfully! (This is just a demo.)');
-        });
+feather.replace();
+
+function togglePwd() {
+    const pwd = document.getElementById('pwd');
+    const eyeOpen = document.getElementById('eyeOpen');
+    const eyeClosed = document.getElementById('eyeClosed');
+    if (pwd.type === 'password') {
+        pwd.type = 'text';
+        eyeOpen.classList.add('hidden');
+        eyeClosed.classList.remove('hidden');
+    } else {
+        pwd.type = 'password';
+        eyeOpen.classList.remove('hidden');
+        eyeClosed.classList.add('hidden');
+    }
+}
+
+function toggleCnfrmPwd() {
+    const pwd = document.getElementById('cnfrmPwd');
+    const eyeOpen = document.getElementById('eyeOpenCnfrm');
+    const eyeClosed = document.getElementById('eyeClosedCnfrm');
+    if (pwd.type === 'password') {
+        pwd.type = 'text';
+        eyeOpen.classList.add('hidden');
+        eyeClosed.classList.remove('hidden');
+    } else {
+        pwd.type = 'password';
+        eyeOpen.classList.remove('hidden');
+        eyeClosed.classList.add('hidden');
+    }
+}
+
+document.getElementById('signupForm').addEventListener('submit', e => {
+    e.preventDefault();
+    alert('Account created successfully! (This is just a demo.)');
+});
